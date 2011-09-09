597ff5992bf7bf20de364c9d6754b1ef83631c8a
buntu <MrFubuntu@googlemail.com>
2011-09-09 14:17:41 +0200

    fixes

--- Global.py
+++ Global.py
@@ -64,10 +64,10 @@ if "state" not in dir():
 	## @brief Current state.
 	 # @details Possible values are "normal" or "modeeditor".
 	state="normal"
-not_a_setting=("CONSOLE_MESSAGE", "CENTER_MESSAGE", "SAY", "QUIT", "EXIT", "SPAWN_ZONE", 
+	not_a_setting=("CONSOLE_MESSAGE", "CENTER_MESSAGE", "SAY", "QUIT", "EXIT", "SPAWN_ZONE", 
                "COLLAPSE_ZONE", "TELEPORT_PLAYER", "RESPAWN_PLAYER", "KICK", "SUSPEND", "SILENCE"
                "ADD_HELP_TOPIC", "REMOVE_HELP_TOPIC", "PLAYER_MESSAGE", "UNBAN_IP","UNSUSPEND", "UNBAN_USER")
-not_a_setting_prefixes=("SPAWN_","REMOVE")
-loadedExtensions=[]
-supportedCommands=[]
-availableExtensions=[]
+	not_a_setting_prefixes=("SPAWN_","REMOVE")
+	loadedExtensions=[]
+	supportedCommands=[]
+	availableExtensions=[]
